b08937e1dfef4ecfe978819e3fc83997031d0254
KimSiJin <kimsijin33@gmail.com>
2025-06-26 15:14:48 +0900

    feat: Creat add function

--- adder.py
+++ adder.py
@@ -1,4 +1,11 @@
 
-for i in range(5):
-    if i%2==0:
-     print(f'{i} hello')
+#for i in range(5):
+#    if i%2==0:
+#     print(f'{i} hello')
+
+def sum(a, b):
+    c = a + b
+    print(c)
+
+if '__main__' ==(__name__):
+    sum(3,2)
